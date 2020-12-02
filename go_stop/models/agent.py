c06d6d1195f19341a24c171db629eba03187bfdb
Keonwoo Kim <keonwoo@kaist.ac.kr>
2020-12-03 03:47:12 +0900

    test on heroku

--- go_stop/models/agent.py
+++ go_stop/models/agent.py
@@ -8,11 +8,13 @@ from abc import ABC
 import random
 from typing import Literal, Optional
 
+from numpy.random import choice
 import torch
 from torch import Tensor
 
 from .action import NUM_ACTIONS, all_actions
 from .game import Game
+from ..train.args import args
 from ..train.encoder import encode_game
 from ..train.network import EncoderNet
 
@@ -47,9 +49,7 @@ class Agent(ABC):
             net.eval()
             with torch.no_grad():
                 encoded_game = (
-                    encode_game(game, game.state.player)
-                    .unsqueeze(0)
-                    .float()
+                    encode_game(game, game.state.player).unsqueeze(0).float()
                 )
                 mask = (
                     Tensor(
@@ -62,14 +62,12 @@ class Agent(ABC):
                 )
 
                 action_index = (
-                    net(encoded_game)[0]
-                    .squeeze()
-                    .masked_fill(mask, 0)
-                    .argmax()
-                    .item()
-                )
+                    net(encoded_game)[0].squeeze().masked_fill(mask, 0)
+                ) ** (1 / args.infinitesimal_tau)
+                action_index = action_index.numpy()
+                action_index = choice(range(NUM_ACTIONS), 1, p=action_index)[0]
 
-                print(game.serialize())
+                print(action_index)
 
                 action = all_actions[action_index]
                 if action not in game.actions():
